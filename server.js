be2d61033a7ff0cf6f32dd2884af14529b218f05
Brynna Miller <brynnam@email.unc.edu>
2023-01-23 20:47:20 -0500

    addition of hostname, working on minimist module

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 // Require http module
 const http = require('http');
+const hostname = '127.0.0.1';
 // Require fs module
 const fs = require('fs');
 // Require minimist module (make sure you install this one via npm).
-var portInput = require('minimist')(process.argv.slice(2)) || 3000;
+const args = require('minimist')(process.argv.slice(2)).port || 3000;
 
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-const port = portInput
+const port = args.port;
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
