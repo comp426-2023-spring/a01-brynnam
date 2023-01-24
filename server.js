e4e10060a8efd35c9b91d239b5417f774f7b37f7
Brynna Miller <brynnam@email.unc.edu>
2023-01-23 20:28:58 -0500

    fixing port

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const http = require('http');
 // Require fs module
 const fs = require('fs');
 // Require minimist module (make sure you install this one via npm).
-const port = require('minimist')(process.argv) || 3000;
+var portInput = require('minimist')(process.argv.slice(2)) || 3000;
 
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-
+const port = portInput
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
